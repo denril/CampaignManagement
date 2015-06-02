d5c6214ccc318dd947d4553a5c3f22aabaf5e68a
Yuriy Yukhymenko <revanushka@yahoo.gr>
2015-06-02 16:41:01 +0300

    CampaignService which invokes converter DTO to entity and saves the entity to repository. **Need to fix the repository save**

---
+++ src/main/java/sepe/service/CampaignService.java
@@ -0,0 +1,52 @@
+package sepe.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import sepe.config.Constants;
+import sepe.config.SpringUserDetails;
+import sepe.domain.*;
+import sepe.dto.*;
+import sepe.dto.converter.*;
+import sepe.handlers.EmailExistsException;
+import sepe.handlers.UsernameExistsException;
+import sepe.repository.*;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.security.SecureRandom;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Created by giouri-adm on 2/6/2015.
+ */
+
+@Service
+public final class CampaignService {
+
+    @Autowired
+    CampaignRepository campaignRepository;
+   // @Autowired
+   // CampaignToCampaignDTO campaignDTOConverter;
+
+
+    public void createCampaign(
+            @Nonnull final CampaignDTO campaign) {
+
+
+        CampaignDTOToCampaign campaignConverter = new CampaignDTOToCampaign();
+        CampaignEntity myCampaign = campaignConverter.convert(campaign);
+
+
+       // campaignRepository.save(myCampaign); //problematic save
+    }
+
+
+}
